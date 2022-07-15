6546cd9b87a902605749fac4354b4974adabe87e
nguyenht <mrlimfo@gmail.com>
2022-07-16 04:54:49 +0700

    delete unnecessary import library at controllers

--- src/java/controller/AdminProductDeleteController.java
+++ src/java/controller/AdminProductDeleteController.java
@@ -7,7 +7,6 @@ package controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
